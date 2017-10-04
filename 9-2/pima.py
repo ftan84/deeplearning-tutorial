cf1d6702180e17c142888f6bdac60a6ef90b665a
Francis Tan <francis.adrian.tan@gmail.com>
2017-10-03 18:51:56 -0700

    Finished

--- 9-2/pima.py
+++ 9-2/pima.py
@@ -40,7 +40,7 @@ grid_result = grid.fit(x, y)
 # Summarize grid search
 print('Best: %f using %s' % (grid_result.best_score_, grid_result.best_params_))
 means = grid_result.cv_results_['mean_test_score']
-stds = grid_results.cv_results_['std_test_score']
+stds = grid_result.cv_results_['std_test_score']
 params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
     print('%f (%f) with: %r' % (mean, stdev, param))
