26ad85b1d391d444ae6c13b8746ab5aa4f239ed6
Francis Tan <francis.adrian.tan@gmail.com>
2017-11-20 21:17:15 -0800

    Added some comments

--- cnn/mnist.py
+++ cnn/mnist.py
@@ -10,7 +10,11 @@ K.set_image_dim_ordering('th')
 
 seed = 120
 # Add comment here
+# Another comment here
 np.random.seed(seed)
+# How about writing a whole
+# long comment here?
+# another comment line
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 num_pixels = x_train.shape[1] * x_train.shape[2]
@@ -18,6 +22,7 @@ x_train = x_train.reshape(x_train.shape[0], num_pixels).astype('float32')
 x_test = x_test.reshape(x_test.shape[0], num_pixels).astype('float32')
 x_train = x_train / 255
 x_test = x_test / 255
+# comment
 
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
